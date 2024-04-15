bddbf9ab58a4396eab4267ed2a9a6325c2fd1554
Vy-X-S <mikhail.ocampo@sjsu.edu>
2024-04-15 13:08:29 -0700

    I/O and primality check

--- lab1.py
+++ lab1.py
@@ -1,7 +1,10 @@
+# Lab 1
+# Mikhail Ocampo | 4/15/2024 | https://github.com/mikhailocampo/CIS22C-lab1
+# Purpose of the assignment is to create a program that determines if an array of integers is prime using iteration and recursion.
+
 import math
 
 def log_activity(func):
-    #
     def wrapper(*args, **kwargs):
         print(f"Entering {func.__name__}")
         try:
@@ -10,19 +13,7 @@ def log_activity(func):
             print(f"Leaving {func.__name__}")
     return wrapper
 
-@log_activity
-def isArrayPrimeIter(ary: list, i_size: int) -> bool:
-    # Param: an array of integers, size of array as int
-    # Post:
-    # Return: True if all elements in array are prime, False otherwise
-    
-    assert isinstance(ary, list)
-    assert all(isinstance(x, int) for x in ary)
-    assert isinstance(i_size, int)
-    
-    if i_size == 0: return True
-    
-    def isNumPrime(num: int) -> bool:
+def isNumPrime(num: int) -> bool:
         # Param: receives an integer and determine if it is prime
         # Return: Bool is array is prime
         
@@ -36,14 +27,63 @@ def isArrayPrimeIter(ary: list, i_size: int) -> bool:
                 return False
             i += 6
         return True
+
+@log_activity
+def isArrayPrimeIter(ary: list, i_size: int) -> bool:
+    # Algorithm checks if all elements in array are prime using iteration
+    # Param: ary - a non-empty array of integers, 
+    #        n - size of array as int
+    # Post:
+    # Return: True or False
+    
+    assert isinstance(ary, list)
+    assert all(isinstance(x, int) for x in ary)
+    assert isinstance(i_size, int)
+    
+    if i_size == 0: return True
     
     for num in ary:
-        if not (isNumPrime(num)):
-            print(f"Not a prime array using iteration.")
-            return False
-    print(f"Array was found to be prime using iteration.")
+        if not (isNumPrime(num)): return False
     return True
 
+@log_activity
+def isArrayPrimeRecur(ary: list, i_size: int) -> bool:
+    # Algorithm checks if all elements in array are prime using recursion
+    # Param: ary - a non-empty array of integers, 
+    #        n - size of array as int
+    # Post:
+    # Return: True or False
+    
+    assert isinstance(ary, list)
+    assert all(isinstance(x, int) for x in ary)
+    assert isinstance(i_size, int)
+    
+    def isPrimeRecur(dividend: int, divisor = 2):
+        if dividend < 2:
+            return False
+        if divisor > int(math.sqrt(dividend)):
+            return True
+        if dividend % divisor == 0:
+            return False
+        return isPrimeRecur(dividend, divisor + 1)
+    
+    if i_size == 0: return True
+    if not isPrimeRecur(ary[i_size - 1]): return False
+    
+    return isArrayPrimeRecur(ary, i_size - 1)
+        
 if __name__ == "__main__":
+    global SORT_MAX_SIZE
     SORT_MAX_SIZE = 8
     
+    print("Please enter your input data on one line only")
+    s = input()
+    numbers = [int(x) for x in s.split()]
+    
+    if isArrayPrimeIter(numbers, len(numbers)):
+        print("Array was found to be prime using iteration.")  
+    else: print("Not a Prime Array using iteration.")  
+    
+    if isArrayPrimeRecur(numbers, len(numbers)):
+        print("Array was found to be prime using recursion.")  
+    else: print("Not a Prime Array using recursion.")  
--- test_prime_check.py
+++ test_prime_check.py
@@ -1,5 +1,5 @@
 import unittest
-from lab1 import isArrayPrimeIter
+from lab1 import isArrayPrimeIter, isArrayPrimeRecur
 
 class TestPrimeArrayChecker(unittest.TestCase):
     def test_empty_array(self):
@@ -22,6 +22,29 @@ class TestPrimeArrayChecker(unittest.TestCase):
     
     def test_large_numbers(self):
         self.assertTrue(isArrayPrimeIter([1000000007, 1000000009, 1000000021], 3), "Array with large primes should return True.")
+
+class TestPrimeArrayCheckerRecursive(unittest.TestCase):
+    def test_empty_array(self):
+        self.assertTrue(isArrayPrimeRecur([], 0), "An empty array should be considered as having all primes.")
+
+    def test_all_primes(self):
+        self.assertTrue(isArrayPrimeRecur([2, 3, 5, 7, 11], 5), "Array with all primes should return True.")
+
+    def test_no_primes(self):
+        self.assertFalse(isArrayPrimeRecur([1, 4, 6, 8, 10], 5), "Array with no primes should return False.")
+
+    def test_mixed_numbers(self):
+        self.assertFalse(isArrayPrimeRecur([2, 3, 4, 5, 6], 5), "Array with both primes and non-primes should return False.")
+
+    def test_with_zero_and_one(self):
+        self.assertFalse(isArrayPrimeRecur([0, 1, 5, 7], 4), "Array containing 0 and 1 should return False.")
+
+    def test_negative_numbers(self):
+        self.assertFalse(isArrayPrimeRecur([-2, -3, -5, -7], 4), "Array with negative numbers should return False.")
+    
+    def test_large_numbers(self):
+        self.assertTrue(isArrayPrimeRecur([1000000007, 1000000009, 1000000021], 3), "Array with large primes should return True.")
+
 
 if __name__ == '__main__':
     unittest.main()
